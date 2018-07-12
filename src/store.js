79588a29375879adc9fefaf8ba8cf4bf6ece24db
Marta Kruneva <marta.kruneva@gmail.com>
2018-07-12 17:26:43 +0100

    chrome redux extension setup

--- src/store.js
+++ src/store.js
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers' // /index
 
@@ -7,6 +7,13 @@ const initialState = {};
 const middleware = [thunk];
 
 // createStore(reducer F, [preloadedState], [enhancer] F);
-const store = createStore(rootReducer, initialState, applyMiddleware(...middleware));
+const store = createStore(
+  rootReducer, 
+  initialState,
+  compose(
+    applyMiddleware(...middleware),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()   // used for Chrome Redux 
+  ) 
+);
 
 export default store;
