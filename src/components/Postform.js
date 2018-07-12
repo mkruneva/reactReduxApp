be9cfe7cee194d32223917826188a318cba438e9
Marta Kruneva <marta.kruneva@gmail.com>
2018-07-12 11:49:35 +0100

    form submit post

--- src/components/Postform.js
+++ src/components/Postform.js
@@ -1,14 +1,50 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import axios from 'axios';
 
 class PostForm extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      title: '',
+      body: ''
+    };
+
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
   }
+
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+    const post = {
+      title: this.state.title,
+      body: this.state.body 
+    }
+    
+    axios.post(`https://jsonplaceholder.typicode.com/posts`, post)
+      .then(res => console.log(res.data))
+  }
+
   render() {
     return (
       <div>
         <h1>Add Post</h1>
+        <form onSubmit={this.onSubmit}>
+          <div>
+            <label>Title: </label><br/>
+            <input type="text" name="title" onChange={this.onChange} value={this.state.title}/>
+          </div>
+          <br />
+          <div>
+            <label>Body: </label><br/>
+            <textarea name="body" onChange={this.onChange} value={this.state.body}></textarea>
+          </div> 
+          <br />
+          <button type="submit">Submit</button>
+        </form>
       </div>
     )
   }
